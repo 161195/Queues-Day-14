ab87f412f413a7a36d8e1186aa9edc3563b4ba6f
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-31 00:05:15 +0530

    adding welcome msg

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Queue_Day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to -World Queue!");
+        }
+    }
+}
