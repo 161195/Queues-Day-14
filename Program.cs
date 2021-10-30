0609234e29d24c425e3d2ca8d8fa0e0e5a0446a2
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-31 00:19:55 +0530

    adding UC1 to remote

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace Queue_Day14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to -World Queue!");
+            QueuesLinkedList queues = new QueuesLinkedList();          //creating the queue object and calling it
+            queues.Enqueue(56);
+            queues.Enqueue(30);
+            queues.Enqueue(70);
+            queues.Display();
         }
     }
 }
---
+++ QueueLinkedList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Day14
+{
+    class QueueLinkedList
+    {
+         
+        internal Node top;                              //instead of head we use top and bottom in stacks
+
+        internal void Push(int data)                   //creating the psuh method which is similar to add first method in linkedlist
+        {
+
+            Node node = new Node(data);                 //creating a node object of the Node class 
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed into the stack ", node.data);
+        }
+
+        internal void Display()          //the display method is similar to the display method of linkeddlist
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        internal void Peek()               //this method tells us about the first element of the stack.it just views.
+        {
+            if (this.top == null)
+                Console.WriteLine("The stacks is empty!");
+            Console.WriteLine("{0} is in the top of the stack.", this.top.data);
+
+        }
+
+        internal void Pop()               //deletes the first node of the stack
+        {
+            if (this.top == null)
+                Console.WriteLine("Deletion is not posssible");
+
+            Console.WriteLine("Deleted node from stack: " + this.top.data);  //prints the deleted node
+            this.top = this.top.next;
+        }
+
+        internal void IsEmpty()          //this repreatedly calls the pop method that will delete the first element from the stack in each iteration
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+    }
+
+    public class QueuesLinkedList
+    {
+        internal Node front;                            //we use front and rear in queus
+
+        internal void Display()          //the display method is similar to the display method of linkeddlist
+        {
+            Node temp = this.front;
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            while (temp != null)
+            {
+                Console.Write("\n" + temp.data);
+                temp = temp.next;
+            }
+        }
+
+        internal void Enqueue(int data)        //the append(enqueue) method it is similar to Add method or Add last
+        {
+            Node node = new Node(data);        //creating a node object of the Node class 
+            if (this.front == null)
+            {
+                this.front = node;
+            }
+            else
+            {
+                Node temp = front;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} added into Queue", node.data);
+        }
+
+
+
+    }
+}
+
